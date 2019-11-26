736a1ee79d3e0c66f68e036beb9f80d0b004aab5
Srikanth B <srikanth_b@live.com>
2019-11-26 22:31:09 +0530

    Updated SQL Q

---
+++ TBT_Blog/Controllers/ContactsToCSVController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TBT_Blog.Controllers
+{
+    public class ContactsToCSVController : Controller
+    {
+        // GET: ContactsToCSV
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
